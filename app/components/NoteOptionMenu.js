34c94dbb0ce4796f0557ee5284fa7f981cd58dc3
Rynare <rynare.of.legend@gmail.com>
2024-03-28 16:57:21 +0700

    [NEW][UI/UX] Menambahkan fitur notes option untuk dapat menghapus dan mengarsipkan note

---
+++ app/components/NoteOptionMenu.js
@@ -0,0 +1,27 @@
+export class NoteOptionMenu extends HTMLElement {
+    constructor() {
+        super()
+    }
+
+
+    connectedCallback() {
+        this.render()
+    }
+
+    render() {
+        const template = document.createElement('template')
+        template.innerHTML = `
+            <i class="${this.getAttribute('bs-icon')}"></i>
+            <span>${this.getAttribute('text')}</span>
+        `
+        this.appendChild(template.content.cloneNode(true))
+
+        this.addEventListener('click', () => {
+            this.dispatchEvent(new CustomEvent('note-option-changed', {
+                detail: { noteOption: this.getAttribute('value') },
+                bubbles: true,
+                composed: true
+            }));
+        })
+    }
+}
