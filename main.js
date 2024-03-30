889e287de1831877729553f0b68e2c81dacc216c
Rynare <rynare.of.legend@gmail.com>
2024-03-31 01:39:58 +0700

    Menghapus comment

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// * Custom elements
 import { MyNotes } from './app/components/MyNotes.js';
 import { NoteList } from './app/components/NoteList.js';
 import { NoteItem } from './app/components/NoteItem.js';
@@ -6,7 +5,6 @@ import { NoteDetail } from './app/components/NoteDetail.js';
 import { SortButton } from './app/components/SortButton.js';
 import { SortItemMode } from './app/components/SortItemMode.js';
 
-// * Custom Elements 
 customElements.define('my-notes', MyNotes, { extends: 'div' });
 customElements.define('note-list', NoteList);
 customElements.define('note-item', NoteItem);
