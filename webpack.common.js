9a585c1c451baac39a9b96b218897c31654caec7
Rynare <rynare.of.legend@gmail.com>
2024-03-31 21:18:50 +0700

    [Update] menambahkan configurasi webpack

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            /* style and css loader */
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            insert: "body",
+                        },
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+            filename: 'index.html',
+        }),
+    ],
+};
